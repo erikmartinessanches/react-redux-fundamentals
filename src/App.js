59767e6bb91a86f553097b2ddadcae75009351af
Erik Martines Sanches <erik.martinessanches@gmail.com>
2021-09-19 11:00:16 +0200

    Writing a reducer, creating and subsribing to a store, incrementing on click.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux';
+/*Reducers
+  Convention: If the reducer receives an undefined state, it should return
+  a suiting initial state of the application. */
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
+
+console.log(store.getState());
+
+/**store.subscribe() lets us register a callback that will be called any time
+ * an action is dispatched.
+ */
+store.subscribe(()=>{
+  document.body.innerText = store.getState(); //Naive.
+});
+
+/**Any time the body is clicked, we'll dispatch an action to increment the 
+ * counter. */
+document.addEventListener('click', () =>{
+  store.dispatch({type: 'INCREMENT'});
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    document.body.innerText = store.getState()
   );
 }
 
