6bfc857b2af9476a2c32809a4c7524e808f47e7c
Erik Martines Sanches <erik.martinessanches@gmail.com>
2021-09-19 12:11:44 +0200

    Simple counter application using just React and Redux. Able to increment and decrement counter, subscribing to a store and updating the UI.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import './App.css';
-import { createStore } from 'redux';
-/*Reducers
-  Convention: If the reducer receives an undefined state, it should return
-  a suiting initial state of the application. */
+//ßimport './App.css';
+// import { createStore } from 'redux';
+// /*Reducers
+//   Convention: If the reducer receives an undefined state, it should return
+//   a suiting initial state of the application. */
 
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    default:
-      return state;
-  }
-}
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return state + 1
+//     case 'DECREMENT':
+//       return state - 1
+//     default:
+//       return state;
+//   }
+// }
 
-const store = createStore(counter);
+// const store = createStore(counter);
 
-console.log(store.getState());
+// console.log(store.getState());
 
-/**store.subscribe() lets us register a callback that will be called any time
- * an action is dispatched.
- */
-store.subscribe(()=>{
-  document.body.innerText = store.getState(); //Naive.
-});
+// /**store.subscribe() lets us register a callback that will be called any time
+//  * an action is dispatched.
+//  */
+// store.subscribe(()=>{
+//   document.body.innerText = store.getState(); //Naive.
+// });
 
-/**Any time the body is clicked, we'll dispatch an action to increment the 
- * counter. */
-document.addEventListener('click', () =>{
-  store.dispatch({type: 'INCREMENT'});
-})
+// /**Any time the body is clicked, we'll dispatch an action to increment the 
+//  * counter. */
+// document.addEventListener('click', () =>{
+//   store.dispatch({type: 'INCREMENT'});
+// })
 
-function App() {
-  return (
-    document.body.innerText = store.getState()
-  );
-}
+// function App() {
+//   return (
+//     document.body.innerText = store.getState()
+//   );
+// }
 
-export default App;
+//export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+//import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+/*Reducers
+  Convention: If the reducer receives an undefined state, it should return
+  a suiting initial state of the application. */
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state;
+  }
+}
+
+const Counter = ({value, onIncrement, onDecrement}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+  
 );
+
+const store = createStore(counterReducer);
+
+/**Here I wrapped ReactDOM.render in my own render function in order to pass 
+ * it to store.subscribe() as a callback. The result is that our page is 
+ * re-rendered every time an action is dispatched.
+ */
+const render = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Counter 
+        value={store.getState()}
+        onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+        onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+      />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+/**store.subscribe() lets me register a callback that will be called any 
+  *time an action is dispatched.
+  */
+ store.subscribe(render);
+render();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
